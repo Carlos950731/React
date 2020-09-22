62b8fcb2dd94b35b54a9505d0afe759d83167aaf
Carlos950731 <cjospitia@hotmail.com>
2020-09-22 18:20:48 -0500

    react

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+export const  App =  () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="content">
+          <p className="a"></p>
+          <div className="">
+          <a className= "" href="#"></a>
+            <a></a>
+          </div>
+          </div>       
+      </header>
+      <section className="section1">
+        <div className="">
+          <h2>ARGES</h2>
+        </div>
+       <nav className="menu">
+         <a>Home</a>
+         <a>About us</a>
+         <a>Services</a>
+         <a>News</a>
+         <a>Proyects</a>
+         <a>Shop</a>
+         <a>Contact us</a>
+       </nav>
+      </section>
+    </div>
+  );
+}
+
+    
