4015a1fa5025421c846dddf2ef3a63414af419ca
Carlos950731 <cjospitia@hotmail.com>
2020-10-20 18:07:11 -0500

    responsive

--- src/components/PageContactus/HeaderContactus/HeaderContacts/HeaderContactus.js
+++ src/components/PageContactus/HeaderContactus/HeaderContacts/HeaderContactus.js
@@ -1,5 +1,7 @@
 import React from "react"
-import { MailOutlined, PhoneOutlined } from '@ant-design/icons';
+import { MailOutlined, PhoneOutlined, DownCircleOutlined, MenuOutlined } from '@ant-design/icons';
+import { Menu, Dropdown } from 'antd';
+
 
 export const HeaderContactus = () => {
     const dummyMenucontact = [
@@ -12,11 +14,72 @@ export const HeaderContactus = () => {
         { id: 7, text: "SHOP" }
 
     ]
+    const menu = (
+        <Menu>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+                    Login
+            </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+                    Registation
+            </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                    English
+            </a>
+            </Menu.Item>
+        </Menu>
+    );
+
+
+    const menusect = (
+        <Menu>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+                    HOME
+            </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+                    ABOUT
+            </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                    COURSES
+            </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                    EVENT
+            </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                    BLOG
+            </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                    CONTACT
+            </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                    SHOP
+            </a>
+            </Menu.Item>
+        </Menu>
+    );
 
     return (
         <div className="HeaderContactus">
             <div className="header-contact">
                 <div className="cont-phone">
+
                     <div className="icon-contact">
                         <PhoneOutlined />
                     </div>
@@ -34,6 +97,11 @@ export const HeaderContactus = () => {
                     <div className="line-conte"></div>
                     <p className="lgn-text">English</p>
                 </div>
+                <Dropdown overlay={menu}>
+                    <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                        <DownCircleOutlined className="ant-iconus" />
+                    </a>
+                </Dropdown>
             </div>
             <section className="sect-menu">
                 <h1 className="title-menu">ARGES</h1>
@@ -44,6 +112,11 @@ export const HeaderContactus = () => {
                             <div className="line-conter"></div>
                         </div>
                     )}
+                    <Dropdown overlay={menusect}>
+                        <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                            <MenuOutlined className="ant-icon" />
+                        </a>
+                    </Dropdown>
                 </div>
 
             </section>
